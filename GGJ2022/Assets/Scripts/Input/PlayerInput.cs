0229df5d9359fbaacc254779694e7eb58bff9139
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2022-01-28 21:31:54 +0100

    Scriptable object example - player input data base layout

---
+++ GGJ2022/Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Player Input", fileName = "playerInput.asset")]
+public class PlayerInput : ScriptableObject
+{
+    public KeyCode m_leftKey = KeyCode.A;
+    public KeyCode m_rightKey = KeyCode.D;
+    public KeyCode m_upKey = KeyCode.W;
+    public KeyCode m_downKey = KeyCode.S;
+}
