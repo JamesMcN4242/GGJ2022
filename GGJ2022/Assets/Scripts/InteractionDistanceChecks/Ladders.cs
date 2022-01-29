43345292ecd3b8f805d81789ac18e06374ce8456
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2022-01-29 21:29:11 +0100

    Add a hacky ladder first basis

---
+++ GGJ2022/Assets/Scripts/InteractionDistanceChecks/Ladders.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Ladders : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            WorldFlags.PlayersOnLadders.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            WorldFlags.PlayersOnLadders.Remove(other.gameObject);
+        }
+    }
+}
--- GGJ2022/Assets/Scripts/StaticHackyScripts/WorldFlags.cs
+++ GGJ2022/Assets/Scripts/StaticHackyScripts/WorldFlags.cs
@@ -1,5 +1,9 @@
-﻿public static class WorldFlags
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class WorldFlags
 {
     public static bool PlayerDead = false;
     public static int ResetValue = 1;
+    public static HashSet<GameObject> PlayersOnLadders = new HashSet<GameObject>();
 }
