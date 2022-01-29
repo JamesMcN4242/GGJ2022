686cd8235562ca46bb618e2f3d436942349d0d95
studentrk <studentrk@outlook.it>
2022-01-29 22:33:19 +0100

    enemies starting

--- GGJ2022/Assets/Scripts/Input/ProjectileController.cs
+++ GGJ2022/Assets/Scripts/Input/ProjectileController.cs
@@ -13,4 +13,18 @@ public class ProjectileController: MonoBehaviour
         {
             transform.position += playerDirection * 3 * Time.deltaTime;
         }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            
+        // Debug.Log("Object with name {other.name} entered the trigger");
+        // var enemy = other.GetComponentInParent<PlayerEntity>();
+        // player.Inventory.Add(gameObject.name);
+        // Object.Destroy(gameObject);
+        }
     }
+}
+    
+    
