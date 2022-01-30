6389d5ae6bd65b6ac43cbd942e143be658e1336d
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2022-01-30 16:02:35 +0100

    Updated to show the amount and count of what you pick up

--- GGJ2022/Assets/Scripts/States/BaseGameState/BaseGameState.cs
+++ GGJ2022/Assets/Scripts/States/BaseGameState/BaseGameState.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using PersonalFramework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,8 +9,11 @@ public class BaseGameState : FlowStateBase
     private PlayerEntity[] m_players = null;
     private EnemyEntity[] m_enemies = null;
     private BaseGameUI m_stateUI = null;
-    private ResetTimer m_resetTimer = new ResetTimer(1000f);
+    private ResetTimer m_resetTimer = new ResetTimer(300f);
     private PortalSystem m_portalSystem = new PortalSystem();
+
+    private int flowersToCollect = 0;
+    private int skullsToCollect = 0;
 
     protected override bool AquireUIFromScene()
     {
@@ -36,6 +40,11 @@ public class BaseGameState : FlowStateBase
             input => new EnemyEntity(input, Resources.Load<EnemyData>($"Data/enemyData/{input.name.Split('(')[0]}")));
 
         mainCam.SetActive(false);
+        m_stateUI.UpdateCollectableImages();
+
+        var collectables = GameObject.FindObjectsOfType<AddToInventory>();
+        flowersToCollect = collectables.Count(i => i.name.Contains("flower"));
+        skullsToCollect = collectables.Length - flowersToCollect; 
     }
 
     protected override void UpdateActiveState()
@@ -61,6 +70,7 @@ public class BaseGameState : FlowStateBase
     protected override void FixedUpdateActiveState()
     {
         m_stateUI.UpdateTimerFill(m_resetTimer.PortionRemaining);
+        m_stateUI.UpdateCollectableText(m_players[0].Inventory.Count, WorldFlags.ResetValue % 2 == 0? skullsToCollect : flowersToCollect, m_players[1].Inventory.Count, WorldFlags.ResetValue % 2 == 0? flowersToCollect:skullsToCollect);
     }
 
     private void ResetLevel()
--- GGJ2022/Assets/Scripts/States/BaseGameState/BaseGameUI.cs
+++ GGJ2022/Assets/Scripts/States/BaseGameState/BaseGameUI.cs
@@ -1,13 +1,33 @@
-﻿using PersonalFramework;
+﻿using System;
+using PersonalFramework;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BaseGameUI : UIStateBase
 {
     [SerializeField] private Image m_clockImage = null;
+    [SerializeField] private Image m_p1Collectables = null;
+    [SerializeField] private Image m_p2Collectables = null;
+    [SerializeField] private TextMeshProUGUI m_p1CollectCount = null;
+    [SerializeField] private TextMeshProUGUI m_p2CollectCount = null;
 
     public void UpdateTimerFill(float fillValue)
     {
         m_clockImage.fillAmount = fillValue;
+    }
+
+    public void UpdateCollectableImages()
+    {
+        if (WorldFlags.ResetValue % 2 == 0)
+        {
+            (m_p1Collectables.sprite, m_p2Collectables.sprite) = (m_p2Collectables.sprite, m_p1Collectables.sprite);
+        }
+    }
+    
+    public void UpdateCollectableText(int player1Collected, int p1Target, int p2Collected, int p2Target)
+    {
+        m_p1CollectCount.text = $"{player1Collected}/{p1Target}";
+        m_p2CollectCount.text = $"{p2Collected}/{p2Target}";
     }
 }
