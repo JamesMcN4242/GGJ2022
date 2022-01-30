606db7b26487692db043ce981499423646f29e34
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2022-01-30 14:03:56 +0100

    Pew pew pitch and starting position hack

--- GGJ2022/Assets/Scripts/AudioPlaying/AudioQuickFire.cs
+++ GGJ2022/Assets/Scripts/AudioPlaying/AudioQuickFire.cs
@@ -9,6 +9,12 @@ public static class AudioQuickFire
         GameObject newObj = Object.Instantiate(k_audioPrefab);
         var source = newObj.GetComponent<AudioSource>();
         source.clip = Resources.Load<AudioClip>(audioName);
+        if (audioName.EndsWith("pewpew"))
+        {
+            source.pitch = Random.Range(1f, 3f);
+            source.time = 1.2f;
+        }
+
         source.Play();
         Object.Destroy(newObj, source.clip.length + 0.1f);
     }
